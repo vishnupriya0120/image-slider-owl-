2d2331b9300207a571011121d3d346a48ba0d2f7
vishnupriya0120 <60998066+vishnupriya0120@users.noreply.github.com>
2022-10-26 23:22:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        dots:true,
+        nav:true,
+        autoplay:true,
+        smartSpeed:2000,
+        autoplayHoverPause:true,
+
+    });
+});
