b65d5db9b1993f7652faeff3675211ce68aafeef
vishnupriya0120 <60998066+vishnupriya0120@users.noreply.github.com>
2022-10-27 21:38:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
+
+    // set owl-carousel width equals to owl-wrapper width
+    function owlWrapperWidth( selector ) {
+        $(selector).each(function(){
+          $(this).find('.owl-carousel').outerWidth( $(this).closest( selector ).innerWidth() );
+        });
+      }
+  
+      // trigger on start and resize
+      owlWrapperWidth( '.owl-wrapper' );
+      $( window ).resize(function() {
+        owlWrapperWidth( $('.owl-wrapper') );
+      });  
     
     $(".owl-carousel").owlCarousel({
         items:1,
         loop:true,
         dots:true,
         nav:true,
-        autoplay:true,
-        smartSpeed:2000,
-        autoplayHoverPause:true,
+        navText:["<img src='images/prev.png'>","<img src='images/next.png'>"],
+        // autoplay:true,
+        // smartSpeed:1500,
+        // autoplayHoverPause:true,
 
     });
 });
